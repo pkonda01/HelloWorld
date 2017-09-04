1e13a0c16519c3c5e7e10265dced4176707f814e
pkonda01 <praveen.vayu@gmail.com>
2017-09-04 01:44:24 -0700

    new files added

--- Hello/HelloWorld.java
+++ Hello/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 
-		for (int i = 1; i < 10; i++) {
+		for (int i = 1; i < 20; i++) {
 			System.out.println("Hello World...." + i);
 		}
 	}
